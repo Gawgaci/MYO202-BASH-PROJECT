a31a54a226e1f9519e904ad46179078c7bf3f88f
Burhan Yavaş <burhanyavas2000@gmail.com>
2026-05-29 15:46:41 +0300

    RAM ve Disk kapasite, seri no bilgileri eklendi

--- main.sh
+++ main.sh
@@ -2,7 +2,7 @@
 # Burhan YAVAŞ
 # 2420191018
 # Docker Temelleri: https://www.btkakademi.gov.tr/portal/certificate/validate?certificateId=ax1hrL07JO
-# Siber Güvenlikte Linux İşletim Sistemleri: https://www.btkakademi.gov.tr/portal/certificate/validate?certificateId=GoDfmP1KD9
+# Siber Güvenlikte Linux: https://www.btkakademi.gov.tr/portal/certificate/validate?certificateId=GoDfmP1KD9
 # Linux Bash Script: https://www.techcareer.net/en/account/courses?tab=completed
 
 date -Iseconds > report.log
@@ -11,6 +11,7 @@ wmic cpu get name >> report.log
 wmic memorychip get capacity >> report.log
 wmic baseboard get product,Manufacturer >> report.log
 wmic csproduct get UUID >> report.log
+wmic diskdrive get model,serialnumber,size >> report.log
 getmac >> report.log
 
 read -s -p "Lütfen parolayı girin: " PAROLA
