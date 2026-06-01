dffec590007ec8b4d0ab4c9bba4830aad7dedbed
Burhan Yavaş <burhanyavas2000@gmail.com>
2026-06-01 13:56:37 +0300

    Donanim detaylari eklendi

--- main.sh
+++ main.sh
@@ -6,11 +6,11 @@
 # Linux Bash Script: https://credsverse.com/credentials/8ad6c2c7-d9c7-4d88-bfb8-32582a3f3a22
 
 date -Iseconds > report.log
-wmic cpu get name >> report.log
-wmic memorychip get capacity >> report.log
-wmic baseboard get product,Manufacturer >> report.log
+wmic cpu get Manufacturer,Name >> report.log
+wmic memorychip get Manufacturer,PartNumber,SerialNumber,Capacity >> report.log
+wmic baseboard get Manufacturer,Product,SerialNumber >> report.log
 wmic csproduct get UUID >> report.log
-wmic diskdrive get model,serialnumber,size >> report.log
+wmic diskdrive get Manufacturer,Model,SerialNumber,Size >> report.log
 getmac >> report.log
 
 read -s -p "Lütfen parolayı girin: " PAROLA
